be3b80e50d644624e5c7913789066a7a7823fc8a
sa304 <satelital2020@protonmail.com>
2020-06-25 17:47:52 -0400

    agrego formulario
    pagina de inicio: login

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,51 @@
       <router-link to="/">Login</router-link> |
       <router-link to="/home">Home</router-link> |
       <router-link to="/favoritos">Favoritos</router-link>
+
+<div class="container mt-5">
+  <input type="text" v-model="email">
+    <input type="text" v-model="password">
+    <button @click="login">Log in</button>
+    <button @click="quienSoy">¿Quien soy?</button>
+    <button @click="logout">Log out</button>
+</div>
     </div>
-    <router-view/>
   </div>
 </template>
+
+<script>
+import Firebase from 'firebase'
+export default {
+  name: 'App',
+  data() {
+    return {
+      email: '',
+      password:' '
+    }
+  },
+  methods:{
+    login(){
+      Firebase.auth().signInWithEmailAndPassword(this.email, this.password).then (
+        accept => {
+          alert('Fuiste logeado! revisa la consola')
+          this.quienSoy
+        },
+        reject => {
+          alert('No mi hermano, usted no existe')
+          console.log(reject.message)
+        }
+      )
+    },
+    logout(){
+      Firebase.auth().signOut()
+    },
+    quienSoy(){
+      let currentUser = Firebase.auth().currentUser
+      console.log(currentUser)
+    }
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,21 @@
 <template>
-  <div class="/login">
-    <h1>bienvenido a login</h1>
+  <div class="/">
+    
+    <form>
+      <input type="text" placeholder="Usuario">
+
+      <input type="password" placeholder="Contraseña">
+
+      <input type="submit" value="Acceder">
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
   </div>
 </template>
