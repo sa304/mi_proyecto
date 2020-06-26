33c8ff0d4e27549d33637fa42ef94eb5e8dbfc05
sa304 <satelital2020@protonmail.com>
2020-06-26 16:05:13 -0400

    agrego vista fuente
    y enlazo componente footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -7,7 +7,7 @@
             <h5 class="mt-1">Sobre el Proyecto</h5>
             <p
               class="text-justify"
-            >Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, eveniet excepturi veritatis sequi ipsa delectus corrupti quos doloribus dolores soluta, consectetur tenetur aspernatur adipisci labore. Quos itaque labore laudantium ea!</p>
+            >  El uso de la API de TVmaze está licenciado por CC BY-SA . Esto significa que los datos se pueden usar libremente para cualquier propósito, siempre que TVmaze se acredite correctamente como fuente y su aplicación cumpla con la disposición de ShareAlike. Puede satisfacer el requisito de atribución al volver a vincular a TVmaze desde su aplicación o sitio web, por ejemplo, utilizando las URL disponibles en la API.</p>
           </div>
           <div class="col-4 mx-auto text-center">
             <h5 class="mt-1">Agradecimientos</h5>
@@ -19,7 +19,7 @@
       </div>
       <hr>
       <div class="footer-copyright text-center py-3">
-        2020 - Sin derechos reservados | Material de uso público
+        2020 | Laberinto TV | hecho con perseverancia
         <a href="#">Proximamente</a>
       </div>
     </footer>
--- src/views/Fuente.vue
+++ src/views/Fuente.vue
@@ -1,27 +1,43 @@
 <template>
   <div id="fuente">
-
-<Navbar /> <!--importa componente-->
-  
-<Footer /> <!--importa componente-->
-  
+    <Navbar />
+    <!--importa componente-->
+<div class="container text-justify">
+  <div class="row">
+       <p class="text-justify mx-5 my-5">
+      <strong>
+        Eres libre de:
+        </strong> 
+     <ul>
+ <li>Compartir : copie y redistribuya el material en cualquier medio o formato</li>
+<li>
+       Adaptar : mezclar, transformar y construir sobre el material
+      para cualquier propósito, incluso comercialmente.
+     </li>
+     </ul>
+        <strong>
+        Importante saber:
+        </strong> 
+         <ul>
+El licenciante no puede revocar estas libertades mientras siga los términos de la licencia.
+     </ul>
+    </p>
+  </div>
+</div>
+    <Footer />
+    <!--importa componente-->
   </div>
 </template>
-
 <script>
 import Navbar from "@/components/Navbar.vue"; /*no borrar*/
-
 import Footer from "@/components/Footer.vue"; /*no borrar*/
-
 export default {
-  name: "Fuente", /*se debe llamar igual*/
+  name: "Fuente" /*se debe llamar igual*/,
   components: {
     Navbar,
     Footer
   }
 };
 </script>
-
 <style lang="scss">
-
 </style>
